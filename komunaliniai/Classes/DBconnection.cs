786ff9d07fdd5944288c7dce75dc48c8f3f018a1
RubinoPD <robertaskask@gmail.com>
2024-01-28 14:02:35 +0200

    Add project files.

---
+++ komunaliniai/Classes/DBconnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace komunaliniai
+{
+    public class DBConnection
+    {
+        public static string GetConnectionString()
+        {            
+            string server = "localhost";
+            string database = "rob";
+            string user = "root";
+            string password = "";
+
+            string connectionString = $"Server={server};Database={database};User Id={user};Password={password};";
+            return connectionString;
+        }
+    }
+}
+    
+
---
+++ komunaliniai/Classes/admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace komunaliniai
+{
+    public class Admin : User
+    {
+        public int AdminID { get; set; }
+        public Admin(string firstName, string lastName, int roleID, int login, int adminID) : base(firstName, lastName, roleID, login)
+        {
+            AdminID = adminID;
+        }
+    }
+}
